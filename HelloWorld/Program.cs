d64d2a9df886e0e250694a783377238155edf960
mackiechimuka <mackiechimuka@gmail.com>
2023-04-23 08:37:55 +0200

    my firstConsoleApp

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class MyApp
+{
+    static void Main(string[] args)
+    {
+        double width, height, woodLength, glassArea;
+        string widthString, heightString;
+
+        string name = "McNeill Chimuka";
+        string location = "Gweru, Zimbabwe";
+        Console.WriteLine($"My name is {name}.I'm from {location}.");
+        DateTime todayDate = DateTime.Now;
+        Console.WriteLine("Current Date:"+todayDate.ToString("MM/dd/yyyy"));
+        DateTime christmas = new DateTime(2023 / 12 / 25);
+        string dateDiff = (christmas.Date - todayDate.Date).Days.ToString();
+        Console.WriteLine($"There are {dateDiff} until christmas ");
+        Console.WriteLine("Enter your width in metres just digit only");
+        widthString = Console.ReadLine();
+        width = Double.Parse(widthString);
+        Console.WriteLine("Enter your height in metres just the digit only ");
+        heightString = Console.ReadLine();
+        height = Double.Parse(heightString);
+        woodLength = 2 * (width + height) * 3.25;
+        glassArea = 2 * (width * height);
+        Console.WriteLine($"The woodlength is {woodLength} feet");
+        Console.WriteLine($"The glassArea is {glassArea} square meters");
+
+        Console.ReadKey();
+    }
+}
